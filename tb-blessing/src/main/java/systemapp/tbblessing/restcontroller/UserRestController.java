ed6a225586328f6375d74da343b3c2550e85ea2e
unknown <martingabriel75@gmail.com>
2020-12-13 19:03:35 +0700

    database connected to heroku add on

--- tb-blessing/src/main/java/systemapp/tbblessing/restcontroller/UserRestController.java
+++ tb-blessing/src/main/java/systemapp/tbblessing/restcontroller/UserRestController.java
@@ -2,7 +2,6 @@ package systemapp.tbblessing.restcontroller;
 
 import systemapp.tbblessing.model.*;
 import systemapp.tbblessing.service.*;
-import systemapp.tbblessing.object.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +11,6 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 import java.util.NoSuchElementException;
 import javax.validation.Valid;
-import org.json.*;
 
 @RestController
 @RequestMapping("api/v1")
